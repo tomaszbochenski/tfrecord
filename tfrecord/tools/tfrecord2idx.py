ab68a635688773904b290482b0cbf0a8000bf2f7
Tomasz Bochenski <tomasz.bochenski@allegro.pl>
2021-07-27 12:38:16 +0200

    Idx for compressed files
    Signed-off-by: Tomasz Bochenski <tomasz.bochenski@allegro.pl>

--- tfrecord/tools/tfrecord2idx.py
+++ tfrecord/tools/tfrecord2idx.py
@@ -2,9 +2,12 @@ from __future__ import print_function
 
 import sys
 import struct
+import gzip
+from typing import Optional
 
 
-def create_index(tfrecord_file: str, index_file: str) -> None:
+def create_index(tfrecord_file: str, index_file: str,
+                 compression_type: Optional[str] = None) -> None:
     """Create index from the tfrecords file.
 
     Stores starting location (byte) and length (in bytes) of each
@@ -18,7 +21,12 @@ def create_index(tfrecord_file: str, index_file: str) -> None:
     index_file: str
         Path where to store the index file.
     """
-    infile = open(tfrecord_file, "rb")
+    if compression_type == "gzip":
+        infile = gzip.open(tfrecord_file, "rb")
+    elif compression_type is None:
+        infile = open(tfrecord_file, "rb")
+    else:
+        raise ValueError("compression_type should be either 'gzip' or None")
     outfile = open(index_file, "w")
 
     while True:
@@ -41,10 +49,13 @@ def create_index(tfrecord_file: str, index_file: str) -> None:
 
 def main():
     if len(sys.argv) < 3:
-        print("Usage: tfrecord2idx <tfrecord path> <index path>")
+        print("Usage: tfrecord2idx <tfrecord path> <index path> <optional compression_type>")
         sys.exit()
 
-    create_index(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 3:
+        create_index(sys.argv[1], sys.argv[2], None)
+    elif len(sys.argv) == 4:
+        create_index(sys.argv[1], sys.argv[2], sys.argv[3])
 
 
 if __name__ == "__main__":
